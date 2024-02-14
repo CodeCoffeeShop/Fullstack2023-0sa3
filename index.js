3673ef8725bb101c5c66bccb6855b3baa668bd69
Gert Salvet <gert@Gerts-MBP.lan>
2024-02-14 13:08:13 +0200

    Parannus edelliselle

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.static('dist'))
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
@@ -101,7 +106,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
